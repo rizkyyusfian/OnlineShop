82ed9ddf27f2196d2f2b088c97e4da3988a079c7
rizkyyusfian <yusfian.zoldyck@gmail.com>
2020-12-04 00:11:10 +0900

    buat fitur login

--- app/src/main/java/com/ubaya/onlineshop/LoginActivity.kt
+++ app/src/main/java/com/ubaya/onlineshop/LoginActivity.kt
@@ -3,6 +3,10 @@ package com.ubaya.onlineshop
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_sign_up.*
@@ -18,6 +22,28 @@ class LoginActivity : AppCompatActivity() {
         }
 
         btnLogin.setOnClickListener {
+            val q = Volley.newRequestQueue(this)
+            val url = "http://ubaya.prototipe.net/nmp160418112/login.php"
+            val stringRequest = object: StringRequest(
+                Request.Method.POST, url,
+                {
+                    Log.d("apiresult", it)
+                    Toast.makeText(this, "Berhasil Login", Toast.LENGTH_LONG).show()
+                    var intentLogin = Intent(this, MainActivity::class.java)
+                    startActivity(intentLogin)
+                },
+                {
+                    Log.d("apiresult", it.message.toString())
+                }
+            ) {
+                override fun getParams(): MutableMap<String, String> {
+                    var params = HashMap<String, String>()
+                    params.put("email", txtLoginEmail.text.toString())
+                    params.put("password", txtLoginPassword.text.toString())
+                    return params
+                }
+            }
+            q.add(stringRequest)
         }
     }
 }
